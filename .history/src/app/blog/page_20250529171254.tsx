93ca5061a0c2f7f1a29d230ca60381dc9dbbc01c
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2025-05-29 17:15:18 +0100

    [fixed blog layout issue]

---
+++ .history/src/app/blog/page_20250529171254.tsx
@@ -0,0 +1,51 @@
+"use client";
+import LogoComponent from "@/components/Minor/Logo";
+import Socials from "@/components/Minor/Socials";
+import styled from "styled-components";
+import { Blogs } from "../../data/BlogData";
+import BlogComponent from "@/components/BlogComponent";
+
+const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+const Container = styled.div`
+  background-color: ${(props) => props.theme.text};
+  width: 100%;
+  height: auto;
+  position: relative;
+  padding-bottom: 5rem;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
+
+const Blog = () => {
+  return (
+    <MainContainer>
+      <Container>
+        <LogoComponent theme={"brown"} />
+        <Socials theme={"brown"} />
+        <Center>
+          <Grid>
+            {Blogs.map((blog) => (
+              <BlogComponent key={blog.id} blog={blog} />
+            ))}
+          </Grid>
+        </Center>
+      </Container>
+    </MainContainer>
+  );
+};
+
+export default Blog;
